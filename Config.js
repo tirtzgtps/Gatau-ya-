10cebec9f1b2c4521aa5efb565c30d1ac2510538
tirtzgtps <katasandinyamanabang@gmail.com>
2025-02-23 14:25:56 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -4,7 +4,7 @@ module.exports = {
     server_data: {
         ip: "142.93.222.5",    // your enet host
         port: "17091",  // your enet port
-        meta: "localhost",
+        meta: "growscythe",
         loginurl: "137.0.0.1"   // login dashboard
     }
 };
